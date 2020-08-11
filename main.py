cbf4a51a4f07fa54863f52b94fa0d72c05f0c764
advaitchorghade <66726242+advaitchorghade@users.noreply.github.com>
2020-08-11 10:49:25 +0100

    initial thoughts of how to execute
    not the most efficient way

--- main.py
+++ main.py
@@ -5,3 +5,17 @@ code2 = ['WTB','whitbread']
 code3 = ['GSK','gee esk kay']
 code4 = ['AZN','astrazeneca']
 code5 =  ['FTSE100','footsie']
+
+while True:
+    price2 = str(stonks.stonks(code2[0],country,0))
+    price2 = (" ".join(price2))
+    price2 = price2.replace(' . ', '.')
+    print(price2)
+    price3 = str(stonks.stonks(code3[0],country,0))
+    price3 = (" ".join(price3))
+    price3 = price3.replace(' . ', '.')
+    print(price3)
+    price4 = str(stonks.stonks(code4[0],country,0))
+    price4 = (" ".join(price4))
+    price4 = price4.replace(' . ', '.')
+    print(price4)
